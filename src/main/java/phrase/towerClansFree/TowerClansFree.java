08580855de2ee9bf411149ea943c2970538b3be4
phrase <veniamintereshchenko@mail.ru>
2025-08-19 14:30:33 +0300

    init

---
+++ src/main/java/phrase/towerClansFree/TowerClansFree.java
@@ -0,0 +1,17 @@
+package phrase.towerClansFree;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TowerClansFree extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
